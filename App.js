7df82bd69f13473d62448a3334bb19019f117b4f
Victor Daniel Guzman Perez <vdgp_12@hotmail.com>
2021-01-20 20:09:20 -0600

    navegación funcionando

--- App.js
+++ App.js
@@ -2,17 +2,16 @@ import React from 'react'
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import { Button } from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
+import Navigation from './src/navigation/Navigation';
 
 export default function App() {
   return (
-   <PaperProvider>
-    <SafeAreaView>
-      <Text>Hola</Text>
-      <Button icon="image" mode="contained" onPress={() => console.log('Pressed')}>
-    Press me
-  </Button>
-    </SafeAreaView>
-  </PaperProvider> 
+    <PaperProvider>
+      <NavigationContainer>
+        <Navigation/>
+      </NavigationContainer>
+    </PaperProvider> 
   )
 }
 
---
+++ src/screens/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {  Text, View } from 'react-native'
+
+export default function Search() {
+    return (
+        <View>
+            <Text>Estamos en Search</Text>
+        </View>
+    )
+}
+
+
