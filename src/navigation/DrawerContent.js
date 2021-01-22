97550d82b331c54665a070df19951f8e0dc2025a
Victor Daniel Guzman Perez <vdgp_12@hotmail.com>
2021-01-21 20:53:58 -0600

    modo oscuro terminado

--- src/navigation/DrawerContent.js
+++ src/navigation/DrawerContent.js
@@ -2,11 +2,13 @@ import React, {useState} from 'react'
 import { StyleSheet, View } from 'react-native'
 import {DrawerContentScrollView} from "@react-navigation/drawer"
 import {Drawer,Switch,TouchableRipple,Text}from 'react-native-paper';
+import usePreference from "../hooks/usePreferences";
 
 //Este componente es para hacer un drawer personalizado, se manda a llamar en Navigation
 export default function DrawerContent(props) {
     const {navigation}= props;
     const [active, setActive] = useState("home");
+    const {theme, toggleTheme} = usePreference();
 
     const onChangeScreen = (screen) => {
         setActive(screen);
@@ -19,8 +21,24 @@ export default function DrawerContent(props) {
                 <Drawer.Item label="Peliculas Populares" active={active === "popular"} onPress={() => onChangeScreen('popular')}/>
                 <Drawer.Item label="Nuevas Peliculas" active={active === "news"} onPress={() => onChangeScreen('news')}/>
             </Drawer.Section>
+            <Drawer.Section title="Opciones">
+                <TouchableRipple>
+                    <View style={styles.preferences}>
+                        <Text>Tema oscuro </Text>
+                        <Switch value={theme === "dark"} onValueChange={toggleTheme}/>
+                    </View>
+                </TouchableRipple>
+            </Drawer.Section >
         </DrawerContentScrollView>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    preferences:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        paddingVertical:12,
+        paddingHorizontal:16,
+    },
+})
